ed7ac8338f71c4e23013c9249ad0f295037f31d4
Octavian Cerna <octavian.cerna@gmail.com>
2013-01-16 02:19:52 +0200

    config.h: Add key for restart and update rules.

--- config.h
+++ config.h
@@ -34,7 +34,8 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Pidgin",   NULL,       NULL,       0,            True,        -1 },
+	{ "qemu-",    NULL,       NULL,       0,            True,        -1 },
 };
 
 /* layout(s) */
@@ -99,6 +100,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask,           XK_r,      quit,           {.i = 1 } }, /* restart */
 };
 
 /* button definitions */
